4993f73fb9b8e5cd627839db67ae77cc32199395
Derek Li <derek.li@mail.utoronto.ca>
2014-12-05 10:22:23 -0800

    added LESS compiler to package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,7 @@ module.exports = function(grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-react');
 
