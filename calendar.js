2ae720c707114d2e6d58919567c23032e8091947
Derek Li <derek.li@mail.utoronto.ca>
2014-12-04 17:47:20 -0800

    instantiated calendar body component

--- calendar.js
+++ calendar.js
@@ -14,15 +14,15 @@
     // Return the number of days in a given month.
     // Month is a value from 0 (January) to 11 (December).
     numberOfDays: function(year, month) {
-      var d = new Date(year, month + 1, 0);
-      return d.getDate();
+      var date = new Date(year, month + 1, 0);
+      return date.getDate();
     },
 
     // Get the day of the week that the first day of a given month falls on.
     // Return a value from 0 (Sunday) to 6 (Saturday).
-    dayOfWeek: function(year, month) {
-      var d = new Date(year, month, 1);
-      return d.getDay();
+    dayOfFirst: function(year, month) {
+      var date = new Date(year, month, 1);
+      return date.getDay();
     },
 
     nextMonth: function() {
@@ -44,18 +44,61 @@
     },
 
     render: function() {
-
-      var dayOfFirst = this.dayOfWeek(this.state.year, this.state.month);
-      var numberOfDays = this.numberOfDays(this.state.year, this.state.month);
+      var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
       return (
         <div>
-          <div>{dayOfFirst}</div>
-          <div>{numberOfDays}</div>
+          <div>{months[this.state.month] + ' ' + this.state.year}</div>
+
           <span>
             <button onClick={this.previousMonth}>Previous month</button>
             <button onClick={this.nextMonth}>Next month</button>
           </span>
+
+          <CalendarBody
+            month={this.state.month}
+            year={this.state.year}
+            dayOfFirst={this.dayOfFirst(this.state.year, this.state.month)}
+            numberOfDays={this.numberOfDays(this.state.year, this.state.month)}
+            daysInPreviousMonth={this.numberOfDays(this.state.year, this.state.month - 1)}
+          />
+        </div>
+      )
+    }
+  });
+
+  var CalendarBody = React.createClass({
+    generateHeading: function() {
+      var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+      var daysHeading = [];
+
+      daysOfWeek.forEach(function(day) {
+        daysHeading.push(
+          <span>{day}</span>
+        );
+      });
+
+      return daysHeading;
+    },
+
+    generateDays: function() {
+
+      var days = [];
+
+      
+
+    },
+
+    render: function() {
+      return (
+        <div>
+          <div>
+            {this.generateHeading()}
+          </div>
+          <div>
+
+          </div>
         </div>
       )
     }
