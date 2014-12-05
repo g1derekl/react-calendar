b6f883e01baf2a2a17bf55f54d6e03534a26f6ed
Derek Li <derek.li@mail.utoronto.ca>
2014-12-04 16:54:16 -0800

    date swicher functions

--- calendar.js
+++ calendar.js
@@ -2,16 +2,63 @@
 
 (function(window) {
 
-  var numberOfDays = function(year, month) {
-    var d = new Date(year, month, 0);
-    return d.getDate();
-  }
+  // The main component, containing all parts of the calendar.
+  window.ReactCalendar = React.createClass({
+    getInitialState: function() {
+      return {
+        year: new Date().getFullYear(),
+        month: new Date().getMonth()
+      }
+    },
 
-  var dayOfWeek = function(year, month) {
-    var d = new Date(year, month, 0);
-    return d.getDay();
-  }
+    // Return the number of days in a given month.
+    // Month is a value from 0 (January) to 11 (December).
+    numberOfDays: function(year, month) {
+      var d = new Date(year, month + 1, 0);
+      return d.getDate();
+    },
 
-  
+    // Get the day of the week that the first day of a given month falls on.
+    // Return a value from 0 (Sunday) to 6 (Saturday).
+    dayOfWeek: function(year, month) {
+      var d = new Date(year, month, 1);
+      return d.getDay();
+    },
+
+    nextMonth: function() {
+      var newMonth = new Date(this.state.year, this.state.month + 1);
+
+      this.setState({
+        year: newMonth.getFullYear(),
+        month: newMonth.getMonth()
+      });
+    },
+
+    previousMonth: function() {
+      var newMonth = new Date(this.state.year, this.state.month - 1);
+
+      this.setState({
+        year: newMonth.getFullYear(),
+        month: newMonth.getMonth()
+      });
+    },
+
+    render: function() {
+
+      var dayOfFirst = this.dayOfWeek(this.state.year, this.state.month);
+      var numberOfDays = this.numberOfDays(this.state.year, this.state.month);
+
+      return (
+        <div>
+          <div>{dayOfFirst}</div>
+          <div>{numberOfDays}</div>
+          <span>
+            <button onClick={this.previousMonth}>Previous month</button>
+            <button onClick={this.nextMonth}>Next month</button>
+          </span>
+        </div>
+      )
+    }
+  });
 
 })(window);
---
+++ example/script.js
@@ -0,0 +1,1 @@
+React.render(React.createElement(ReactCalendar, null), document.body);
