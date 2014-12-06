a7dd874dae2cf42cff3ae975bb2e862836bf5193
Derek Li <derek.li@mail.utoronto.ca>
2014-12-05 16:39:06 -0800

    ellipses

--- example/script.js
+++ example/script.js
@@ -4,14 +4,21 @@
 
   var date = new Date();
 
-  for (var i=0; i < 1000; i++) { // Procedurally generate 10,000 event objects, 10 each day for 1000 days.
-    for (var j=0; j < 10; j++) {
-      events.push({
-        date: new Date(date.getFullYear(), date.getMonth(), date.getDay() + i),
-        title: 'Event ' + i + '-' + j
-      });
+  // for (var i=0; i < 1000; i++) { // Procedurally generate 10,000 event objects, 10 each day for 1000 days.
+  //   for (var j=0; j < 10; j++) {
+  //     events.push({
+  //       date: new Date(date.getFullYear(), date.getMonth(), date.getDay() + i),
+  //       title: 'Event ' + i + '-' + j
+  //     });
+  //   }
+  // }
+
+  events = [
+    {
+      date: new Date(date.getFullYear(), date.getMonth(), date.getDay()),
+      title: 'This is an extremely long event name that will probably be truncated'
     }
-  }
+  ];
 
   React.render(React.createElement(ReactCalendar, {events: events}), document.body);
 })();
